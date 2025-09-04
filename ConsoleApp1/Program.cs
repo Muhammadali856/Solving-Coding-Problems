0792bc7d3efe559f0fa4a31a680c321d3e7e1ce3
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-09-04 20:08:56 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿class Rectangle
+{
+    public int x = 10, y = 20;
+    public int GetAreaRectangle(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+class Square : Rectangle
+{
+    public int GetAreaSquare(Rectangle rectangle) {
+        return rectangle.GetAreaRectangle(x, y);
+    }
+
+    static void Main(string[] args)
+    {
+        Square square = new Square();
+        int area = square.GetAreaSquare(square);
+        Console.WriteLine($"Area of the square: {area}");
+    }
+}
