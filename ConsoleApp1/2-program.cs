00ea5cee347393d37d1a32e4793325602b53b3d3
MUHAMMADALI-USER <eshboevmuhammadali2@gmail.com>
2025-09-06 12:55:40 +0800

    Codewars: Converting boolean into "Yes" or "No"

---
+++ ConsoleApp1/2-program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1;
+
+namespace ConsoleApp2
+{
+    public class _2_program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Solution.boolToWord(false));
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,23 +1,10 @@
-﻿public class Solution
+﻿namespace ConsoleApp1
 {
-    public int FindClosest(int x, int y, int z)
+    public static class Solution
     {
-        int dx = Math.Abs(x - z);
-        int dy = Math.Abs(y - z);
-
-        if (dx == dy) return 0;
-        return dx < dy ? 1 : 2;
-    }
-}
-
-class Testing : Solution
-{
-    static void Main()
-    {
-        var answer = new Solution();
-        Console.WriteLine(answer.FindClosest(2, 10, 10));
-        Console.WriteLine(answer.FindClosest(4, 3, 2));
-        Console.WriteLine(answer.FindClosest(12, 34, 16));
-        Console.WriteLine(answer.FindClosest(1, 1, 2));
+        public static string boolToWord(bool word)
+        {
+            return word ? "Yes" : "No";
+        }
     }
 }
